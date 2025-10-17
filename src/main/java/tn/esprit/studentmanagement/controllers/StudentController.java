988bfa6acddea13475405e0cdfb0e8c272201144
Emrane23 <omranklaai@gmail.com>
2025-10-17 16:38:19 +0200

    first commit in emraneKLAAI-2MPAWI-G4 branch

--- src/main/java/tn/esprit/studentmanagement/controllers/StudentController.java
+++ src/main/java/tn/esprit/studentmanagement/controllers/StudentController.java
@@ -31,5 +31,5 @@ IStudentService studentService;
 
     @DeleteMapping("/deleteStudent/{id}")
     public void deleteStudent(@PathVariable Long id) { studentService.deleteStudent(id); }
-    //simple changement pour first commit a ma branche
+    //simple changement pour first commit a ma branche 
 }
