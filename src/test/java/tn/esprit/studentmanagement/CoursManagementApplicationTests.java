13e3efcecdf643626a5f7183ea27f633ad5fdf46
iheb-heni <ihebheni013@gmail.com>
2025-12-03 12:18:26 +0100

    fix code bugs

--- src/test/java/tn/esprit/studentmanagement/CoursManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/CoursManagementApplicationTests.java
@@ -3,6 +3,7 @@ package tn.esprit.studentmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import jakarta.transaction.Transactional;
 import tn.esprit.studentmanagement.StudentManagementApplication; // 👈 import main app
@@ -10,6 +11,7 @@ import tn.esprit.studentmanagement.StudentManagementApplication; // 👈 import 
 @SpringBootTest
 @Transactional // rollback after each test
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test") // this will load application-test.properties
 class CoursManagementApplicationTests {
 
     @Test
